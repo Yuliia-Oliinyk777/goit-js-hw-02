0d5741d1a7e0d141d6f1fbdbe9a7021af3d4622b
Yuliia-Oliinyk777 <yuliya2474@gmail.com>
2024-06-15 17:30:21 +0200

    add task-4,variant-2

--- js/task-4.js
+++ js/task-4.js
@@ -6,7 +6,7 @@ function getShippingCost(country) {
             break;
         }  
         case 'Chile': {
- message = 'Shipping to Chile will cost 250 credits';
+            message = 'Shipping to Chile will cost 250 credits';
             break;
         }
         case 'Australia': {
@@ -21,8 +21,32 @@ function getShippingCost(country) {
             message = 'Sorry, there is no delivery to your country'
     }
     }
-    return message;
+     return message;
 }
+
+//Підкажіть, будь - ласка, який варіант краще використовувати ?   
+/*
+function getShippingCost(country) {
+    switch (country) {
+        case 'China': {
+             return 'Shipping to China will cost 100 credits';
+        }  
+        case 'Chile': {
+             return 'Shipping to Chile will cost 250 credits'; 
+        }
+        case 'Australia': {
+            return 'Shipping to Australia will cost 170 credits';
+            
+        }   
+        case 'Jamaica': {
+            return 'Shipping to Jamaica will cost 120 credits';
+        }   
+        default: {
+            return 'Sorry, there is no delivery to your country'
+    }
+    }
+}
+    */
 console.log(getShippingCost("Australia")); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingCost("Germany")); // "Sorry, there is no delivery to your country"
 console.log(getShippingCost("China")); // "Shipping to China will cost 100 credits"
